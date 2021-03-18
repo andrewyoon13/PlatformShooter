69f91cd88c82130042dbd87b86b64824d2a54cbd
csam11 <csam@uwo.ca>
2021-03-18 12:11:00 -0700

    new char big area

--- RPG game/Assets/Scripts/Movement.cs
+++ RPG game/Assets/Scripts/Movement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Rigidbody2D rb;
+    public float speed;
+    private float moveInput;
+    public float jumpForce;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate(){
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
     }
+
+    void Update(){
+
+        isGrounded = Physics2D.OverlapCircle(feetPos.position,checkRadius, whatIsGround);
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space)){
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
 }
