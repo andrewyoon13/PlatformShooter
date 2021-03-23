3889a52d789ed79be7920d16f3b121ee74d07b27
andrewyoon13 <andrewyoon13@gmail.com>
2021-03-23 16:44:12 -0600

    Character Selection working now
    uses 2 different scenes for the 2 different characters

---
+++ RPG game/Assets/SetScene2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SetScene2 : MonoBehaviour
+{
+    public void StartGame2()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
