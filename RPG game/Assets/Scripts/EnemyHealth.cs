88e92aaa20f6f4a2a50f7b88446ebd653bc27255
csam11 <csam@uwo.ca>
2021-03-23 19:49:04 -0700

    finished melee level

--- RPG game/Assets/Scripts/EnemyHealth.cs
+++ RPG game/Assets/Scripts/EnemyHealth.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    
+    public float m_Thrust = 20f;
     public int maxHealth = 100;
     int currentHealth;
+
+    
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+    
     }   
 
     public void TakeDamage(int damage){
+
+
         currentHealth -= damage;
 
         if (currentHealth<=0){
