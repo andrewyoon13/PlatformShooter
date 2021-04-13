b4f19951bb7ea42c997fd39c9f0a011f5a91a616
csam11 <csam@uwo.ca>
2021-04-12 21:23:06 -0700

    tweaks

--- RPG game/Assets/Scripts/MeleeCombat.cs
+++ RPG game/Assets/Scripts/MeleeCombat.cs
@@ -14,7 +14,7 @@ public class MeleeCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.J)){
             Attack();
         }
     }
