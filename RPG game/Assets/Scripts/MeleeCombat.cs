938fdffa6a853810c14716a942e9f92db40e703b
Navid Khan <navidahmedkhan7@gmail.com>
2021-04-12 23:50:06 -0400

    Revert "Enemy Movement"
    This reverts commit 3645d25f1c8ce374f989ea54c6aad3cc5eff8b39, reversing
    changes made to aacde5bdfd70a84ea95b97096faf09f14205e72d.

--- RPG game/Assets/Scripts/MeleeCombat.cs
+++ RPG game/Assets/Scripts/MeleeCombat.cs
@@ -14,7 +14,7 @@ public class MeleeCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.J)){
+        if (Input.GetKeyDown(KeyCode.Space)){
             Attack();
         }
     }
